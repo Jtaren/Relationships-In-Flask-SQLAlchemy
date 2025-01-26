7b4f47e196764914f6b753f13366df1365445480
jtaren <arenjatau@gmail.com>
2025-01-26 18:53:11 +0100

    app

---
+++ One-To-Many/app.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_sqlachemy import SQLAlchemy
+
+app = Flask(__name__)
+
+database_name = one_to_many
+database_path = "postgresql://{}:{}@{}/{}".format("postgres", "eazye5000", "localhost:5432", database_path)
+app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+db = SQLAlchemy(app)
+
+class Owner(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100))
+    address = db.Column(db.String(100))
+    pets = db.relationship('Pet', backref='owner')
+
+class Pet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    age = db.Column(db.Integer)
+    owner_id = db.Column(db.Integer, db.Foreign_key('owner.id'))
