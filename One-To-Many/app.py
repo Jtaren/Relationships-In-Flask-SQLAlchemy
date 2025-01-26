c7151dbaae750e8e843488f1a68e08bec6e50389
jtaren <arenjatau@gmail.com>
2025-01-26 19:35:16 +0100

    app.py

--- One-To-Many/app.py
+++ One-To-Many/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
-from flask_sqlachemy import SQLAlchemy
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 
-database_name = one_to_many
-database_path = "postgresql://{}:{}@{}/{}".format("postgres", "eazye5000", "localhost:5432", database_path)
+database_name = "one_to_many"
+database_path = "postgresql://{}:{}@{}/{}".format("postgres", "eazye5000", "localhost:5432", database_name)
 app.config['SQLALCHEMY_DATABASE_URI'] = database_path
+app.config['SQLALCHEMY_TRACK_MODIFICATIONs'] = False
 db = SQLAlchemy(app)
 
 class Owner(db.Model):
@@ -18,4 +19,4 @@ class Pet(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(20))
     age = db.Column(db.Integer)
-    owner_id = db.Column(db.Integer, db.Foreign_key('owner.id'))
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'))
